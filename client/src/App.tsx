94b5a90c6b26f6301bf3638d44742b5b1ddf13be
Craig McClanahan <craigmcc@gmail.com>
2022-05-31 11:05:08 -0700

    Copy relevant static files, begin on App.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,57 @@
+// App -----------------------------------------------------------------------
+
+// Overall implementation of the entire client application.
+
+// External Modules ----------------------------------------------------------
+
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {ToastContainer} from "react-toastify";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
+
+// Internal Modules ----------------------------------------------------------
+
+import {LoginContextProvider} from "./components/login/LoginContext";
+
+// Component Details ---------------------------------------------------------
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+
+        <ToastContainer
+            autoClose={5000}
+            closeOnClick={true}
+            draggable={false}
+            hideProgressBar={false}
+            newestOnTop={false}
+            position="top-right"
+            theme="colored"
+        />
+
+        <LoginContextProvider>
+          <div className="App">
+            <header className="App-header">
+              <p>
+                Edit <code>src/App.tsx</code> and save to reload.
+              </p>
+              <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </header>
+          </div>
+        </LoginContextProvider>
+
+      </>
   );
+
 }
 
 export default App;
