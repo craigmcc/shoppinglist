a9a2c413f738da3f90c53a17322c7edb34e261bc
Craig McClanahan <craigmcc@gmail.com>
2022-06-07 14:56:10 -0700

    Bump shared-react to pick up fix for SearchBar. Delete the oldclient directory as we no longer need those obsolete files.

--- oldclient/src/constants.ts
+++
@@ -1,8 +0,0 @@
-// constants -----------------------------------------------------------------
-
-// Manifest constants shared across the application.
-
-// Public Objects ------------------------------------------------------------
-
-export const LOGIN_CONTEXT_DATA_KEY = "LOGIN_CONTEXT_DATA";
-export const LOGIN_CONTEXT_USER_KEY = "LOGIN_CONTEXT_USER";
