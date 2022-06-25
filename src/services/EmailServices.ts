da30a5c83afc3d97370bfac98b8860641b4e87d7
Craig McClanahan <craigmcc@gmail.com>
2022-06-25 12:45:27 -0700

    Provide a send() method on EmailServices for general use.

--- src/services/EmailServices.ts
+++ src/services/EmailServices.ts
@@ -5,9 +5,11 @@
 // External Modules ----------------------------------------------------------
 
 import {IncomingMail} from "cloudmailin";
+import Mail from "nodemailer/lib/mailer";
 
 // Internal Modules ----------------------------------------------------------
 
+import {sendMessage} from "./GMailServices";
 import logger from "../util/ServerLogger";
 
 // Public Objects ------------------------------------------------------------
@@ -15,10 +17,21 @@ import logger from "../util/ServerLogger";
 class EmailServices {
 
     public async receive(message: IncomingMail): Promise<void> {
-        console.log("INCOMING MESSAGE", message);
+        logger.info({
+            context: "EmailServices.receive",
+            message: message,
+        });
+        // TODO - dispatch to a handler somehow
+    }
+
+    public async send(message: Mail.Options): Promise<void> {
+        logger.info({
+            context: "EmailServices.send",
+            message: message,
+        })
+        await sendMessage(message);
     }
 
 }
 
 export default new EmailServices();
-
